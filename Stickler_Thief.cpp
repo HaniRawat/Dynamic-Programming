f1eb4b85ba7928bbbb1d1d274e838d7bdb9bc306
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-10-17 23:07:45 +0530

    solved

--- Stickler_Thief.cpp
+++ Stickler_Thief.cpp
@@ -9,16 +9,19 @@
 //         return dp[index];
 //     }
 
-    int solveTabu(int nums[], int n){
-        vector<int> dp(n+1, -1);
+    int solveTab(int arr[],int n)
+    {
+        vector<int> dp(n,0);
+        dp[0] =arr[0];
         
-        dp[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            int op1 = nums[i] + dp[i+2];
-            int op2 = 0 + dp[i+1];
-            dp[i] = max(op1, op2);
+        for(int i=1;i<n;i++)
+        {
+            int incl = dp[i-2] + arr[i];
+            int excl = dp[i-1] +0;
+            dp[i] = max(incl,excl);
         }
-        return dp[0];
+        
+        return dp[n-1];
     }
     
     int FindMaxSum(int arr[], int n)
@@ -26,6 +29,6 @@
         // Your code here
         // vector<int> dp(n+1, -1);
         // int ans = solve(arr, n, 0, dp);
-        int ans = solveTabu(arr, n);
-        return ans;
+        vector<int> dp(n,-1);
+        return solveTab(arr,n);
     }
